bca4fd642e261ece1ca16a9d1e6b618fa7a7d313
Kelly <ksm@kelly-900X3C-900X3D-900X4C-900X4D.(none)>
2013-12-21 12:05:14 -0500

    Adding base files

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<div id="footer">
+        <p>Copyright &copy 2010 <a href="http://www.1stwebdesigner.com">1stWebDesigner.com PHP Template</a></p>
+</div> <!-- end #footer -->
+
---
+++ variables/variables.php
@@ -0,0 +1,5 @@
+<?php
+  
+$heading = 'Lovely Meraki';
+
+?>
