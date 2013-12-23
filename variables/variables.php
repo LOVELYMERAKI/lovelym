4710983d14e52ebd75e7f9d3c5c076e4b4f8f2bc
Kelly <ksm@kelly-900X3C-900X3D-900X4C-900X4D.(none)>
2013-12-22 22:37:32 -0500

    y

--- variables/variables.php
+++ variables/variables.php
@@ -1,6 +1,6 @@
 <?php
   
-$heading = '<img src="img/c2.png" class="cloud-2""><div class="title">Lovely Meraki</div>';
+$heading = '<img src="img/k2.png" class="sleepy-kitty"><img src="img/c2.png" class="cloud-2""><div class="title">Lovely Meraki</div>';
 
 $nav1 = '<div class="nav-1 nav-a"><img src="img/k1.png" class="kitty"><a href="#">About</a></div>';
 $nav2 = '<div class="nav-2 nav-a"><a href="#">Specs</a><img src="img/k3.png" class="kitty"></div>';
